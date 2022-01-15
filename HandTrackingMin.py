36107ba85b6ecbf7436ffd02c61fd6877df67df7
charliebrunold <charliebrunold@gmail.com>
2022-01-14 19:20:21 -0800

    Fixed LED interface, LEDs now light up with hand detection

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -1,12 +1,14 @@
 import cv2
 import mediapipe as mp
 import time
-import RPi.GPIO as GPIO   
+import RPi.GPIO as GPIO
 
-GPIO.setwarnings(False)    
-GPIO.setmode(GPIO.BOARD)   
-GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW)   # Set pin 23 to be an output pin and set initial value to low (off)
-GPIO.setup(24, GPIO.OUT, initial=GPIO.HIGH)   # Set pin 24 to be an output pin and set initial value to HIGH (on)
+LED_GREEN = 18
+LED_RED = 16
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(LED_GREEN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LED_RED, GPIO.OUT, initial=GPIO.HIGH)
 
 cap = cv2.VideoCapture(0)
 
@@ -17,31 +19,37 @@ mpDraw = mp.solutions.drawing_utils
 pTime = 0
 cTime = 0
 
-while True:
-    success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-    # print(results.multi_hand_landmarks)
+try:
+    while True:
+        success, img = cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
 
-    if results.multi_hand_landmarks:
-        GPIO.output(24, GPIO.LOW) 
-        GPIO.output(23, GPIO.HIGH)
-        for handLms in results.multi_hand_landmarks:
-            for id, lm in enumerate(handLms.landmark):
-                # print(id, lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                print(id, cx, cy)
-                if id == 8:
-                    cv2.circle(img, (cx, cy), 15, (255, 0 ,255), cv2.FILLED)
+        if results.multi_hand_landmarks:
+            GPIO.output(LED_RED, GPIO.LOW)
+            GPIO.output(LED_GREEN, GPIO.HIGH)
+            for handLms in results.multi_hand_landmarks:
+                # for id, lm in enumerate(handLms.landmark):
+                    # print(id, lm)
+                    # h, w, c = img.shape
+                    # cx, cy = int(lm.x * w), int(lm.y * h)
+                    # print(id, cx, cy)
+                    # if id == 8:
+                        # cv2.circle(img, (cx, cy), 15, (255, 0 ,255), cv2.FILLED)
 
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+        else:
+            GPIO.output(LED_RED, GPIO.HIGH)
+            GPIO.output(LED_GREEN, GPIO.LOW)
 
-    cTime = time.time()
-    fps = 1/(cTime - pTime)
-    pTime = cTime
+        cTime = time.time()
+        fps = 1/(cTime - pTime)
+        pTime = cTime
 
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+except KeyboardInterrupt: 
+    GPIO.cleanup()
